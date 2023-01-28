166dc93dd4850fa671a07ab677f398c2d7692ae2
PavelDobCZ23 <pavel23dob@gmail.com>
2023-01-28 19:28:30 +0100

    dfdf

--- web-page/script.js
+++ web-page/script.js
@@ -2,8 +2,8 @@ document.getElementById('generate-pack').addEventListener('submit',(eventData) =
     eventData.preventDefault();
     const formData = new FormData(eventData.target);
     console.log(formData);
-    fetch('/generate-pack',{
-        method: 'POST',
-        body: formData
-    })
+    //fetch('/generate-pack',{
+    //    method: 'POST',
+    //    body: formData
+    //})
 })
